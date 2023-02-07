3e3d2a6513c74bcd9d18e545b64c985b24103735
karim-hammami <hammamik790@gmail.com>
2023-02-07 12:57:44 +0100

    add CLI arguments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 const figlet = require("figlet");
+const { Command } = require("commander"); // add this line
 
-console.log(figlet.textSync("Dir Manager"));
+
+const program = new Command();
+
+
+console.log(figlet.textSync("Todo-CLI"));
+
+
+program
+  .version("1.0.0")
+  .description("a todo list cli tool")
+  .option("-l, --List", "List all todo item")
+  .option("-m, --Todo <type...>", "create a todo item")
+  .option("-li, --TodoById <type>", "list a single todo item by id")
+  .option("-u, --Update <type...>", "update a single todo item by id")
+  .option("-d, --Delete <type>", "delete a single todo item by id")
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.Todo) console.log(options.Todo);
+if (options.List) console.log("all todos");
+if (options.TodoById) console.log(options.TodoById);
+if (options.Update) console.log(options.Update);
+if (options.Delete) console.log(options.Delete);
